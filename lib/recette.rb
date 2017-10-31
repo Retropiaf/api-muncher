455e809eed90178a6e30635a13a0da9c1b8042b7
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-31 16:53:14 -0700

    adds link to original recipe

--- lib/recette.rb
+++ lib/recette.rb
@@ -1,14 +1,16 @@
 class Recette
-  attr_reader :title, :image
+  attr_reader :title, :image, :url
   NO_IMAGE = "https://i5.walmartimages.com/asr/f752abb3-1b49-4f99-b68a-7c4d77b45b40_1.39d6c524f6033c7c58bd073db1b99786.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF"
 
   def initialize(hash)
     raise ArgumentError if hash["label"] == nil || hash["label"] == ""
+
     @title = hash["label"]
-    if hash["image"] == nil
-      @image = NO_IMAGE
-    else
-      @image = hash["image"]
-    end
+
+    hash["image"] ? @image = hash["image"] : @image = NO_IMAGE
+
+    hash["url"] ? @url = hash["url"] : @url = www.google.com
+
   end
+
 end
