986ade58b27c4098d6c2f68a6801f6aaaa9154aa
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-31 21:28:28 -0700

    Adds dietary and health labels, adds photo to show page

--- lib/recette.rb
+++ lib/recette.rb
@@ -1,5 +1,5 @@
 class Recette
-  attr_reader :title, :image, :url, :ingredients, :calories, :nutrients, :dailyneed
+  attr_reader :title, :image, :url, :ingredients, :calories, :nutrients, :dailyneed, :source, :diet_labels, :health_labels, :allergies
   NO_IMAGE = "https://i5.walmartimages.com/asr/f752abb3-1b49-4f99-b68a-7c4d77b45b40_1.39d6c524f6033c7c58bd073db1b99786.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF"
 
   def initialize(hash)
@@ -11,10 +11,17 @@ class Recette
 
     hash["url"] ? @url = hash["url"] : @url = www.google.com
 
+    hash["source"] ? @source = hash["source"] : @source = nil
+
+    hash["dietLabels"] ? @diet_labels = hash["dietLabels"] : @diet_labels = nil
+
+    hash["healthLabels"] ? @health_labels = hash["healthLabels"] : @health_labels = nil
+
     hash["ingredientLines"].count > 0 ? @ingredients = hash["ingredientLines"] : @ingredients = ["No ingredient found"]
 
-    calories = hash["calories"]
+    hash["cautions"].count > 0 ? @allergies = hash["cautions"] : @allergies = nil
 
+    calories = hash["calories"]
     if hash["yield"]
       if calories
         @calories = (hash["calories"].to_f/hash["yield"].to_f).to_i.to_s + " calories per serving"
@@ -30,13 +37,13 @@ class Recette
     end
 
     if hash["digest"].empty?
-      @nutrients = "No Nutritional info"
+      @nutrients = {}
     else
       @nutrients = hash["digest"]
     end
 
     if hash["totalDaily"].empty?
-      @dailyneed = "N/A"
+      @dailyneed = {}
     else
       @dailyneed = hash["totalDaily"]
     end
