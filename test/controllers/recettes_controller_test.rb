12d229f3ad809037eee492357dcf6f77ef1067b8
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-11-01 11:56:31 -0700

    Controller tests passing

--- test/controllers/recettes_controller_test.rb
+++ test/controllers/recettes_controller_test.rb
@@ -1,14 +1,19 @@
 require 'test_helper'
 
 class RecettesControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get recettes_index_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get show" do
-  #   get recettes_show_url
-  #   assert_response :success
-  # end
+  describe RecettesController do
+    it "can get index" do
+      VCR.use_cassette("index_action") do
+        get root_path
+        must_respond_with :success
+      end
+    end
+    it "can get show" do
+      VCR.use_cassette("index_action") do
+        get recettes_path("Apple Chips")
+        must_respond_with :success
+      end
+    end
+  end
 
 end
