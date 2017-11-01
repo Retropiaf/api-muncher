97182d476f4547a28f2eae2a8a60487dfb5300e6
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-31 20:32:09 -0700

    Add table of nutrients on show page

--- lib/recette.rb
+++ lib/recette.rb
@@ -1,5 +1,5 @@
 class Recette
-  attr_reader :title, :image, :url, :ingredients, :calories, :nutrients
+  attr_reader :title, :image, :url, :ingredients, :calories, :nutrients, :dailyneed
   NO_IMAGE = "https://i5.walmartimages.com/asr/f752abb3-1b49-4f99-b68a-7c4d77b45b40_1.39d6c524f6033c7c58bd073db1b99786.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF"
 
   def initialize(hash)
@@ -33,6 +33,12 @@ class Recette
       @nutrients = "No Nutritional info"
     else
       @nutrients = hash["digest"]
+    end
+
+    if hash["totalDaily"].empty?
+      @dailyneed = "N/A"
+    else
+      @dailyneed = hash["totalDaily"]
     end
 
   end
