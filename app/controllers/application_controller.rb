b7b94c9b1a6d64a1978a8b26e4a45df2baa92694
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-11-06 09:34:46 -0800

    fixed heroki fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 require_dependency '../../lib/edamam_api_wrapper'
-require_dependency '../../lib/recipe'
+require_dependency '../../lib/recette'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
